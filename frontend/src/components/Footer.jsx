c5c78cf9fc9201ca4f712e652d9cd86d93e878b3
tusharpatel0504 <tushar789patel@gmail.com>
2025-12-11 16:20:12 +0530

    home done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,16 +3,16 @@ import { assets } from '../assets/assets_frontend/assets'
 const Footer = () => {
     return (
         <div  className='md:mx-10'>
-            <div className=''>
+            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
                 {/*left */}
                 <div>
-                    <img src={assets.logo} alt="" />
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <img className='mb-5 w-40' src={assets.logo} alt="" />
+                    <p className='w-full md:w-2/3 text-gray-600 leading-6 '>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
                 {/*center */}
                 <div>
-                    <p>COMPANY</p>
-                    <ul>
+                    <p className='text-xl font-medium mb-5'>COMPANY</p>
+                    <ul className='flex flex-col gap-2 text-gray-600'>
                         <li>Home</li>
                         <li>About us</li>
                         <li>Contact us</li>
@@ -21,8 +21,8 @@ const Footer = () => {
                 </div>
                 {/*right */}
                 <div>
-                    <p>GET IN TOUCH</p>
-                    <ul>
+                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                    <ul className='flex flex-col gap-2 text-gray-600'>
                         <li>+1-212-456-7890</li>
                         <li>greatstackdev@gmail.com</li>
                     </ul>
@@ -31,7 +31,7 @@ const Footer = () => {
             <div>
                 {/*copy right text */}
                 <hr />
-                <p>Copyright © 2024 GreatStack - All Right Reserved.</p>
+                <p className='py-5 text-sm text-center'>Copyright © 2024 GreatStack - All Right Reserved.</p>
             </div>
         </div>
     )
